167de2b5295ab2db86114297a1b0b52e1e93a039
David Hale <david.hale7@gmail.com>
2018-03-31 12:09:38 -0700

    add BooksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'House Of Leaves'},
+    { title: 'Siddhartha'},
+    { title: 'The Sun Also Rises'},
+    { title: 'Still Life With Woodpecker'}
+  ]
+}
