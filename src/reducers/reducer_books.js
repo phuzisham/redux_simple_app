d3f355eaa18c3b1eca2a8c272df7b91d43c656f6
David Hale <david.hale7@gmail.com>
2018-04-05 15:21:30 -0700

    fixed object error and added key to books

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,12 @@
 export default function() {
   return [
-    { title: 'House Of Leaves'},
-    { title: 'Siddhartha'},
-    { title: 'The Sun Also Rises'},
-    { title: 'Still Life With Woodpecker'}
+    { title: 'House Of Leaves',
+      pages: 1000 },
+    { title: 'Siddhartha',
+      pages: 23 },
+    { title: 'The Sun Also Rises',
+      pages: 2},
+    { title: 'Still Life With Woodpecker',
+      pages: 99999999999499099992}
   ]
 }
